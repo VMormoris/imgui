70da66fd682344afc00fd84bd3fe42555c404329
VMormoris <vmormoris2014@gmail.com>
2020-10-29 13:12:53 +0200

    Creating premake5

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    runtime "Release"
+	optimize "on"
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
